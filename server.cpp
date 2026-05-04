353ffd3e951dc34881edb522284706c77a70a898
Arsen <arsend704@gmail.com>
2026-05-04 17:56:50 +0400

    Little example of server socket

---
+++ server.cpp
@@ -0,0 +1,49 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int server_fd;
+    int client_fd;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t          client_len = sizeof(client_addr);
+    char               client_ip[INET_ADDRSTRLEN];
+
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Failed to create server socket");
+        exit(EXIT_FAILURE);
+    }
+
+
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_port        = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+        perror("Failed to bind server socket with address");
+        exit(EXIT_FAILURE);
+    }
+    if(listen(server_fd, 5) < 0){
+        perror("Failed to create server socket listen queue");
+        exit(EXIT_FAILURE);
+    }
+    if((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len)) < 0){
+        perror("Failed to create server socket");
+        exit(EXIT_FAILURE);
+    }
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+    printf("Print clinet ip: %s, port: %d", client_ip, ntohs(client_addr.sin_port));
+
+    close(server_fd);
+    close(client_fd);
+}
+
+
